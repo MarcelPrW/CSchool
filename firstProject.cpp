5cc0c30bded617d0d38ec39e16579d8de8745ca1
MarcelPrW <marcelwojdat9@gmail.com>
2022-02-01 12:08:36 +0100

    second commit

---
+++ firstProject.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cdstio>
+
+using namespace std;
+
+int liczba1, liczba2, wynik;
+int main() {
+    std::cout << "test for string and number 123";
+    std::cout << "test for string and number 123";
+    std::cout << "test for string and number 123";
+    std::cout << "test for string and number 123";
+    std::cout << "test for string and number 123";
+    std::cout << "test for string and number 123";
+    return 0;
+}
